b9f0a344c4b3c8619132f08f5a59ace31e1bc7e9
Sergey Petrunin <neyasut@gmail.com>
2017-02-01 19:23:56 +0200

    add cart show page

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,22 @@
+require 'securerandom'
+
+
+class CartsController < ApplicationController
+  before_action :set_cart, only: [:show, :edit, :update, :destroy]
+
+  # GET /carts/1
+  def show
+  end
+
+  # DELETE /carts/1
+  def destroy
+    @cart.destroy
+    redirect_to carts_url, notice: 'Cart was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_cart
+      @cart = Cart.find_or_initialize_by(cart_hash: session[:current_cart_hash])
+    end
+end
