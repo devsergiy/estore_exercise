4b0a8ef8f17e1f280e416689657838d7248ea3fe
Sergey Petrunin <neyasut@gmail.com>
2017-02-01 18:43:49 +0200

    add count to cart item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170201160119) do
+ActiveRecord::Schema.define(version: 20170201164305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170201160119) do
     t.integer  "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "count"
     t.index ["cart_id"], name: "index_cart_items_on_cart_id", using: :btree
     t.index ["product_id"], name: "index_cart_items_on_product_id", using: :btree
   end
